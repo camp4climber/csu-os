52fba55a31d35962554bb337d818ff92a1067578
camp4climber <camp4climber@gmail.com>
2015-11-12 15:49:53 -0800

    added and init queue.

--- CSCI340-DNS-Name-Resolution-Engine/DNS-handout/multi-lookup.c
+++ CSCI340-DNS-Name-Resolution-Engine/DNS-handout/multi-lookup.c
@@ -2,16 +2,28 @@
 #include <string.h>
 #include "multi-lookup.h"
 
+queue hostQueue;
 pthread_mutex_t queueMutex;
 pthread_mutex_t outputMutex;
 
 int main(int argc, char *argv[])
 {
+	pthread_mutex_init(&queueMutex, NULL);
+	pthread_mutex_init(&outputMutex, NULL);
+	
+	queueInit(%hostQueue, 100);
+
 	if (argc < MIN_ARGS)
 	{
-		printf("Usage:\n%s\n", USAGE);
+		printf("Too Few Arguments.\nUsage: %s\n", USAGE);
 		return 1;
 	}
+	if (argc > MAX_INPUT_FILES + 1)
+	{
+		printf("Too Many Arguments.\nMax is 10.\nUsage: %s\n", USAGE);
+		return 1;
+	} 
+
 	return 0;
 }
 
--- CSCI340-DNS-Name-Resolution-Engine/DNS-handout/multi-lookup.h
+++ CSCI340-DNS-Name-Resolution-Engine/DNS-handout/multi-lookup.h
@@ -3,6 +3,9 @@
 
 #define MIN_ARGS 3
 #define USAGE "./multi-lookup <inputFilePath> ... <outputFilePath>"
-#define SBUFSIZE 1025
 #define INPUTFS "%1024s"
+#define MAX_INPUT_FILES 10
+#define MAX_RESOLVER_THREADS 10
 #define MIN_RESOLVER_THREADS 2
+#define MAX_NAME_LENGTH 1025
+#define MAX_IP_LENGTH INET6_ADDRSTRLEN
