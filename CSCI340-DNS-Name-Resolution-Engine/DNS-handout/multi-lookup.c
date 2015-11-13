b521e7f25381027128b97532aa43597d0bd0c278
camp4climber <camp4climber@gmail.com>
2015-11-12 17:05:34 -0800

    added pseudo code for requester.

--- CSCI340-DNS-Name-Resolution-Engine/DNS-handout/multi-lookup.c
+++ CSCI340-DNS-Name-Resolution-Engine/DNS-handout/multi-lookup.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 #include "multi-lookup.h"
+#include "queue.h"
 
-queue hostQueue;
+queue requestQueue;
 pthread_mutex_t queueMutex;
 pthread_mutex_t outputMutex;
 
@@ -11,7 +13,7 @@ int main(int argc, char *argv[])
 	pthread_mutex_init(&queueMutex, NULL);
 	pthread_mutex_init(&outputMutex, NULL);
 	
-	queueInit(%hostQueue, 100);
+	queue_init(&requestQueue, 100);
 
 	if (argc < MIN_ARGS)
 	{
@@ -27,10 +29,27 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+//Reading the input file and putting stuff in the queue.
+//We'll have one thread per file.
 void requester(char *file_name)
 {
+	//open file
+	//scan file
+		//while(1)
+			//lock mutex
+			//if q full
+				//unlock mutex
+				//sleep
+			//else
+				//push to the queue
+				//unlock mutex
+				//break
+	//close file
 }
 
+//Going through our queue and resolving domains to their IP.
+//We'll have a minimum of 2 threads doing this.
 void resolver()
 {
+
 }
